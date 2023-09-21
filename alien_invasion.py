f406ac9d86ef544d1e376c71248c5d9cbc2339a4
Vitaliy <raziel100@mail.ru>
2023-09-21 21:09:50 +0500

    Start project

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,22 +1,26 @@
 import sys
-
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
-    """Класс для управления ресурсами и поведением игры."""
 
     def __init__(self):
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
-        """Запуск основного цикла игры."""
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             pygame.display.flip()
 
